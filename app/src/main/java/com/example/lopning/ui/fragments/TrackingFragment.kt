85ef522a85f7c33f1c9dca06c1301520e6f8f9f8
Alvaro Isea <alvaroe9448@gmail.com>
2020-09-21 14:10:34 +0200

    MapView is now visible

--- app/src/main/java/com/example/lopning/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/lopning/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,68 @@
 package com.example.lopning.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.lopning.R
 import com.example.lopning.ui.viewModels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking)  {
 
     //by viewModels makes possible to inject viewModel class using Hilt
     private val viewModel: MainViewModel by viewModels()
+
+    private var map : GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initMapView(savedInstanceState)
+
+    }
+
+    private fun initMapView(savedInstanceState: Bundle?) {
+        mapView?.onCreate(savedInstanceState)
+        mapView.getMapAsync{
+            map = it
+        }
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+
 }
