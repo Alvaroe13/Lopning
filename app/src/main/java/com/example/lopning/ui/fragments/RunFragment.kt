bdff3463276d67ad6a7a2c142f3d736c56bf982e
Alvaro Isea <alvaroe9448@gmail.com>
2020-09-21 12:20:56 +0200

    Location Permission with EasyPermission setup done

--- app/src/main/java/com/example/lopning/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/lopning/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.example.lopning.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,21 +9,73 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.lopning.R
 import com.example.lopning.ui.viewModels.MainViewModel
+import com.example.lopning.utils.Constants.LOCATION_REQUEST_CODE
+import com.example.lopning.utils.PermissionHelper
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run)  {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
-    //by viewModels makes possible to inject viewModel class using Hilt
+    //"by viewModels" makes possible to inject viewModel class using Hilt
     private val viewModel: MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermission()
+
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun requestPermission(){
+
+        //if permissions are granted
+        if (PermissionHelper.generalPermissions(requireContext()) ){
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.requestPermissions(
+                this,
+                "These permissions are necessary to use this app.",
+                LOCATION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }else{
+            EasyPermissions.requestPermissions(
+                this,
+                "These permissions are necessary to use this app.",
+                LOCATION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION //needed only in OS Q onward
+            )
+        }
+    }
+
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        }else{
+            requestPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>){}
+
+
+    //framework type function
+    override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //EasyPermission is not framework type function so we pass as param the info needed for it to work
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
--- app/src/main/java/com/example/lopning/utils/Constants.kt
+++ app/src/main/java/com/example/lopning/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.lopning.utils
 object Constants {
 
     const val  DATABASE_NAME  = "Running_DB"
+    const val LOCATION_REQUEST_CODE = 10
 }
